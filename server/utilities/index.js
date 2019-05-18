09bbe0d01c41d48f6633193af7b10e03404306ed
Srettha Terananont(win) <strayedpeople@gmail.com>
2019-05-19 00:00:27 +0700

    add new utilities for routes and error

---
+++ server/utilities/index.js
@@ -0,0 +1,78 @@
+const HttpStatus = require('http-status');
+const _ = require('lodash');
+
+class BattleshipError extends Error {
+    constructor(message, statusCode = HttpStatus.BAD_GATEWAY) {
+        super(message);
+        this.name = this.constructor.name;
+        this.statusCode = statusCode;
+    }
+}
+
+/**
+ * Check whether given function is async or not
+ * @param {Function} fn
+ * @returns {Boolean}
+ */
+function isAsyncFunction(fn) {
+    return typeof fn === 'function' && fn.constructor && fn.constructor.name === 'AsyncFunction';
+}
+
+/**
+ * Wrap async function with try...catch
+ * @param {Function}
+ * @returns {Function}
+ */
+function wrap(fn) {
+    if (!isAsyncFunction(fn)) {
+        return fn;
+    }
+
+    if (fn.length === 4) {
+        return (err, req, res, next) => fn(err, req, res, next).catch(next);
+    }
+
+    return (req, res, next) => fn(req, res, next).catch(next);
+}
+
+/**
+ * Wrap async functions with try...catch
+ * @param {Array<Function>} fn
+ * @returns {Array<Function>}
+ */
+function wrapAll(fns) {
+    return _.map(fns, fn => wrap(fn));
+}
+
+/**
+ * Set routes to router
+ * @param {Router} router
+ * @param {Object} routes
+ * @param {Object} options
+ * @returns {void}
+ */
+function setRoutes(router, routes) {
+    _.forEach(routes, (route) => {
+        if (!route.handler) {
+            return;
+        }
+
+        let args = [];
+
+        if (route.middleware) {
+            args.push(wrapAll(router.middleware));
+        }
+
+        if (route.handler) {
+            args.push(wrap(route.handler));
+        }
+
+        args = _.flatten(args);
+        router[route.method.toLowerCase()](route.path, ...args);
+    });
+}
+
+module.exports = {
+    setRoutes,
+    BattleshipError,
+};
