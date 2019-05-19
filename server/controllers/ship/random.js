832337aff1cb09180fae7892f922814afb2ec44d
Srettha Terananont(win) <strayedpeople@gmail.com>
2019-05-19 16:40:32 +0700

    remove random controller

--- server/controllers/ship/random.js
+++
@@ -1,7 +0,0 @@
-const HttpStatus = require('http-status');
-
-module.exports = async (_req, res) => {
-    res.status(HttpStatus.CREATED).json({
-        message: 'Randomly placed ship on the ocean',
-    });
-};
